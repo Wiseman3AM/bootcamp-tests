ac85fd9313978e99eba8df30cefa87760ee2870c
Wiseman3AM <amukelani210@gmail.com>
2024-07-04 17:27:58 +0200

    last commit

--- test/yearsAgo.tests.js
+++ test/yearsAgo.tests.js
@@ -2,7 +2,7 @@ describe('yearsAgo' , function(){
     it('returns how many years it is from a specific year' , function(){
         assert.equal((new Date().getFullYear() - 1976), yearsAgo(1976))
         assert.equal((new Date().getFullYear() - 2000), yearsAgo(2000))
-        assert.equal((new Date().getFullYear() - 2024), yearsAgo(2024));
+        assert.equal((new Date().getFullYear() - 1830), yearsAgo(1830));
     });
 
 });
