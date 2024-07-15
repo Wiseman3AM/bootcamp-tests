a4a98f03ff8f7241b4cd9b6f525cdd89373ea369
Wiseman3AM <amukelani210@gmail.com>
2024-07-15 12:23:07 +0200

    Final commit

--- test/isWeekDay.tests.js
+++ test/isWeekDay.tests.js
@@ -1,12 +1,24 @@
-describe('isWeekDay' , function(){
-    it('Finds out if the parameter passed in is a day of the week ' , function(){
-        assert.equal(isWeekday('Saturday'), false);
-        assert.equal(isWeekday('Sunday'), false);
-        assert.equal(isWeekday('Monday'), true);
-        assert.equal(isWeekday('Tuesday'), true);
-        assert.equal(isWeekday('Wednesday'), true);
-        assert.equal(isWeekday('Thursday'), true);
-        assert.equal(isWeekday('Friday'), true);
+describe('isWeekDay', function () {
+    it('Finds out if the parameter passed in is a day of the week ', function () {
+        assert.equal(isWeekday('Saturday'), false)
     });
-
+    it('Finds out if the parameter passed in is a day of the week ', function () {
+        assert.equal(isWeekday('Sunday'), false)
+    });
+    it('Finds out if the parameter passed in is a day of the week ', function () {
+        assert.equal(isWeekday('Monday'), true)
+    });
+    it('Finds out if the parameter passed in is a day of the week ', function () {
+        assert.equal(isWeekday('Tuesday'), true)
+    });
+    it('Finds out if the parameter passed in is a day of the week ', function () {
+        assert.equal(isWeekday('Wednesday'), true)
+    });
+    it('Finds out if the parameter passed in is a day of the week ', function () {
+        assert.equal(isWeekday('Thursday'), true)
+    });
+    it('Finds out if the parameter passed in is a day of the week ', function () {
+        assert.equal(isWeekday('Friday'), true)
+    });
 });
+
